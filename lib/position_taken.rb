7af55caf3ea4b2f7f7056ec09dfd1e6179f4e31c
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 19:48:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
-    return true
-  else
     return false
+  else
+    return true
   end
 end
